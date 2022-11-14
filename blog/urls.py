d3d820931e3bbef39baab0f2809dedc430972117
guli2103 <gulbahorcoder@gmail.com>
2022-11-14 23:53:44 +0500

    video

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cube/',include('cube.urls')),
     path('cars/',include('cars.urls')),
+    path('core/', include('core.urls'))
     
 ]
--- core/views.py
+++ core/views.py
@@ -3,9 +3,4 @@ from frontend import *
 
 def video(request):
     return render(request, "index.html")
-
-def ferrary(request):
-    return render(request, "index1.html")
-
-def cube(request):
-    return render(request,"cube.html")    
+  
